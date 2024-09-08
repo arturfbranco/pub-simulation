d7c11153446aad72419aaf8d47101e80a4e57ebc
Artur Branco <arturfdb@gmail.com>
2024-09-08 14:51:45 -0300

    use data from external files

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
+import { readFileSync } from "fs";
 import { Pub } from "./pub";
+import { resolve } from "path";
 
-const endingTime = 50;
-const log = false;
+const endingTime = process.argv[2] ? parseInt(process.argv[2]) : 100;
+const log = process.argv[3] === 'log';
 
-// Arrivals
-const table36 = [1, 10, 15, 6, 2, 2, 2, 1, 11, 0, 5, 13, 6, 0, 11, 5, 1, 20, 4, 12];
+const readData = (table: string) => readFileSync(resolve(__dirname, `../data/${table}`), 'utf-8').split('\n').map(Number);
 
-// Thirst
-const table37 = [4, 2, 1, 2, 2, 1, 2, 2, 1, 2, 4, 1, 3, 3, 4, 4, 1, 2, 4, 1];
+const arrivals = readData('table_3_6');
+const thirsts = readData('table_3_7');
+const drinkingTimes = readData('table_3_8');
+const servingTimes = readData('table_3_9');
 
-// Drinking time
-const table38 = [7, 7, 6, 7, 7, 8, 8, 6, 8, 8, 8, 7, 8, 5, 8, 8, 6, 6, 5, 5];
-
-// Serving time
-const table39 = [5, 5, 6, 5, 5, 5, 6, 6, 6, 6, 3, 5, 7, 5, 6, 6, 7, 6, 6, 7]
-
-if(table36.length !== table37.length || table37.length !== table38.length || table38.length !== table39.length) {
-    throw new Error(`All tables must have the same length. Got ${table36.length}, ${table37.length}, ${table38.length}, ${table39.length}`);
+if(arrivals.length !== thirsts.length || thirsts.length !== drinkingTimes.length || drinkingTimes.length !== servingTimes.length) {
+    throw new Error(`All tables must have the same length. Got ${arrivals.length}, ${thirsts.length}, ${drinkingTimes.length}, ${servingTimes.length}`);
 }
 
-const pub = new Pub(table36, table39, table38, table37, log);
+const pub = new Pub(arrivals, servingTimes, drinkingTimes, thirsts, log);
 
 
 pub.printStats();
